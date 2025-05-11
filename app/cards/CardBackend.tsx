ee8e37cf9b4c30987630df6d5f670fd61c2d18ca
Flonne Reverie <flonne@flonne.dev>
2025-05-11 05:44:04 +0100

    Fixing linter issues

--- app/cards/CardBackend.tsx
+++ app/cards/CardBackend.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import styles from "./CardBackend.module.css";
 
 import Card from '../card';
 
@@ -8,7 +6,7 @@ export default function CardBackend() {
   return (
     <Card>
         <h3>Backend and Sysadmin</h3>
-        <p>This site is hosted on an <a href="https://ubuntu.com/server" rel="nofollow noreferrer" target="_blank">Ubuntu Linux</a> KVM running <a href="https://nginx.org/" rel="nofollow noreferrer" target="_blank">nginx</a>, and encrypted using <a href="https://letsencrypt.org/" rel="nofollow noreferrer" target="_blank">Let's Encrypt</a> and <a href="https://certbot.eff.org/" rel="nofollow noreferrer" target="_blank">Certbot</a>.</p>
+        <p>This site is hosted on an <a href="https://ubuntu.com/server" rel="nofollow noreferrer" target="_blank">Ubuntu Linux</a> KVM running <a href="https://nginx.org/" rel="nofollow noreferrer" target="_blank">nginx</a>, and encrypted using <a href="https://letsencrypt.org/" rel="nofollow noreferrer" target="_blank">Let&apos;s Encrypt</a> and <a href="https://certbot.eff.org/" rel="nofollow noreferrer" target="_blank">Certbot</a>.</p>
         <p>I hand-configured every aspect of this, and ongoingly maintain it.</p>
     </Card>
   );
